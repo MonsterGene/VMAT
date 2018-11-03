8ce4c692903a39bf56b20c88200520f937ee2458
Monster <monster.j.zhang@mail.foxconn.com>
2018-11-03 13:52:55 +0800

    update

--- src/pages/project/menu.js
+++ src/pages/project/menu.js
@@ -1,11 +1,11 @@
 const menu = [
   {
-    title: 'Project',
+    title: 'Demo Project',
     group: 'project',
     component: 'project',
     icon: 'view_compact',
     items: [
-      { name: 'project/page1', title: 'Project Demopage', component: 'project/page1' }
+      { name: 'project/page1', title: 'Demo page', component: 'project/page1' }
     ]
   },
 ];
--- src/pages/project/path.js
+++ src/pages/project/path.js
@@ -1,12 +1,14 @@
-const router = [{
-  path: '/project/page1',
-  meta: { breadcrumb: true },
-  name: 'project/page1',
-  component: () => import(
-    /* webpackChunkName: "routes" */
-    /* webpackMode: "lazy-once" */
-    `./pages/project.vue`
-  )
-}];
+const router = [
+  {
+    path: '/project/page1',
+    meta: { breadcrumb: true },
+    name: 'project/page1',
+    component: () => import(
+      /* webpackChunkName: "routes" */
+      /* webpackMode: "lazy-once" */
+      `./pages/project.vue`
+    )
+  }
+];
 
 export default router;
