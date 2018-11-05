b9f2180bbe472af76133f420adc9f1ec6bc54ce8
Monster <monster.j.zhang@mail.foxconn.com>
2018-11-05 08:27:51 +0800

    update

---
+++ src/pages/project/pages/template.vue
@@ -0,0 +1,18 @@
+<template>
+<v-container grid-list-xl fluid>
+
+</v-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+
+</style>
