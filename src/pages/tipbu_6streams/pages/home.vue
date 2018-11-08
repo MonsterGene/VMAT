1baf8710c3b5fb10eeb980d7a00bd6e391face35
Monster <monster.j.zhang@mail.foxconn.com>
2018-11-08 17:53:47 +0800

    update

--- src/pages/tipbu_6streams/pages/home.vue
+++ src/pages/tipbu_6streams/pages/home.vue
@@ -710,23 +710,6 @@
         </v-flex>
 
       </v-layout>
-
-      <v-layout row wrap>
-        <v-flex v-for="(item, index) in equipment_list" :key="index" lg1 sm2>
-          <v-card color="purple" dark>
-            <v-card-title class="pb-0">
-              <h4>{{ item }}</h4>
-            </v-card-title>
-            <v-card-text>
-              U: 20pcs/h 
-              E: 85%
-            </v-card-text>
-            <v-card-actions>
-              <v-btn flat small>Link</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-flex>
-      </v-layout>
     </v-container>
   </div>
 </template>
@@ -747,7 +730,6 @@ export default {
   },
   data () {
     return {
-      equipment_list: ['equip 1', 'equip 2', 'equip 3', 'equip 4', 'equip 5', 'equip 6', 'equip 7'],
       selectedTab: 'tab-1',
       option: null,
       dataset: {
---
+++ src/pages/tipbu_6streams/pages/line.vue
@@ -0,0 +1,34 @@
+<template>
+<v-container grid-list-xl fluid>
+  <v-layout row wrap>
+    <v-flex v-for="(item, index) in equipment_list" :key="index" lg1 sm2>
+      <v-card color="purple" dark>
+        <v-card-title class="pb-0">
+          <h4>{{ item }}</h4>
+        </v-card-title>
+        <v-card-text>
+          U: 20pcs/h 
+          E: 85%
+        </v-card-text>
+        <v-card-actions>
+          <v-btn flat small to="/tipbu-6streams/line-details">Link</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</v-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      equipment_list: ['equip 1', 'equip 2', 'equip 3', 'equip 4', 'equip 5', 'equip 6', 'equip 7']
+    };
+  }
+};
+</script>
+
+<style lang='stylus' scoped>
+
+</style>
