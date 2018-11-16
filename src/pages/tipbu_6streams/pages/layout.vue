c407a141758ad8d6af33499183ddf72654046060
Monster <monster.j.zhang@mail.foxconn.com>
2018-11-16 14:10:04 +0800

    增加车间layout页面

---
+++ src/pages/tipbu_6streams/pages/layout.vue
@@ -0,0 +1,20 @@
+<template>
+<v-container grid-list-xl fluid>
+  <a href="#/tipbu-6streams/line" style="width:100%">
+    <img src="../static/pic/layout.png" style="width:100%" alt="加载中...">
+  </a>
+</v-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    };
+  }
+};
+</script>
+
+<style lang='stylus' scoped>
+
+</style>
