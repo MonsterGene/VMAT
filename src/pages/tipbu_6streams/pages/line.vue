6da5109e4dd1a9e926dd04d3b31eb4cbef257cc7
Monster <monster.j.zhang@mail.foxconn.com>
2018-11-16 16:13:16 +0800

    line 页面更新

--- src/pages/tipbu_6streams/pages/line.vue
+++ src/pages/tipbu_6streams/pages/line.vue
@@ -2,7 +2,7 @@
 <v-container grid-list-xl fluid>
   <v-layout row wrap>
     <v-flex xs12>
-      <img src="../static/pic/line.png" alt="加载中...">
+      <!-- <img src="../static/pic/line.png" alt="加载中..."> -->
     </v-flex>
   </v-layout>
   <v-btn color="green">正常</v-btn>
@@ -12,7 +12,7 @@
   <v-btn color="black" dark>关机</v-btn>
   <v-layout row wrap style="margin:0">
     <v-card
-      class="station-card line-card"
+      class="line-card"
       color="green"
       dark hover
       href="#/tipbu-6streams/line-details/A01"
@@ -25,21 +25,17 @@
         <p>达成：90%</p>
       </v-card-text>
     </v-card>
-    <v-card
+    <div
       class="station-card"
       v-for="(item, index) in equipment_list"
-      :key="index"
-      color="green"
-      dark hover
-      :href="'#/tipbu-6streams/station-details/'+item"
+      :key="item"
+      style="position:relative;margin-top:50px"
     >
-      <v-card-title class="pb-0">
-        <h4>{{ item }}</h4>
-      </v-card-title>
-      <v-card-text>
-        当日产出： 1500
-      </v-card-text>
-    </v-card>
+      <div style="position:absolute;top:-50px;">
+        <h5>{{ item }}<div style="width:15px;height:15px;background:#4caf50;display:inline-block"></div></h5>
+        <p>产出： 1500</p>
+      </div>
+    </div>
   </v-layout>
 </v-container>
 </template>
@@ -57,7 +53,10 @@ export default {
 <style lang='stylus' scoped>
 .station-card
   margin 5px
-  width 135px
+  width 88px
+  height 139px
+  background-image url('../static/pic/station.png')
+
 .line-card
   border 3px solid #76FF03!important //green lighten-2
 </style>
