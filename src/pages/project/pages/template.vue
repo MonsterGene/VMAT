275ccfade5fb05490dfb20d0328bc3f0cc64f960
M <f1331865@atm.systemplus.foxconn.com>
2018-11-28 15:52:52 +0800

    更新示例

--- src/pages/project/pages/template.vue
+++ src/pages/project/pages/template.vue
@@ -5,19 +5,23 @@
 </template>
 
 <script>
-// import { demoApi, pageApi } from '../api';
-// fetch legends data 使用api接口代码示例
-pageApi.lineApi.getStateLegends().then(response => {
-  // 你的处理程序
-  // response 是请求的响应信息 response
-  // 后台实际返回的数据 res.data
-});
+/**
+ * import { demoApi, pageApi } from '../api';
+ * fetch legends data 使用api接口代码示例
+ * pageApi.lineApi.getStateLegends().then(response => {
+ *   //你的处理程序
+ *   //response 是请求的响应信息 response
+ *   //后台实际返回的数据 res.data
+ * });
+ */
+
+
 export default {
   data () {
     return {
-    }
+    };
   }
-}
+};
 </script>
 
 <style lang='stylus' scoped>
