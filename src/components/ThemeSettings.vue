64e49fb72f03b4005a09b49afe4cd89755c86996
M <f1331865@atm.systemplus.foxconn.com>
2018-11-28 15:37:13 +0800

    修改默认主题颜色

--- src/components/ThemeSettings.vue
+++ src/components/ThemeSettings.vue
@@ -53,7 +53,7 @@ import colors from 'vuetify/es5/util/colors';
 export default {
   data () {
     return {
-      themeColor: 'orange',
+      themeColor: 'blue',
       sideBarOption: 'light',
       colors: colors
     };
