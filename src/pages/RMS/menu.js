60ab2441bcfe6cdca3c72704fa76a7328673c4ca
f1332012 <1318743947@qq.com>
2018-12-11 16:44:24 +0800

    update rms

--- src/pages/RMS/menu.js
+++ src/pages/RMS/menu.js
@@ -6,144 +6,77 @@ const menu = [
     items: [
       {
         title: 'WIP管理&品質反饋',
-        group: 'wip',
-        component: 'rms',
+        group: '/wip',
+        icon: 'view_compact',
         items: [
-          { 
-            name: 'rms/page1', title: 'WIP trend & Quality feed back', component: 'rms/page1'
-          },
-          { 
-            name: 'rms/page2', title: 'WIP detail status', component: 'rms/page2'
-          },
-          { 
-            name: 'rms/page3', title: 'Check-in/out', component: 'rms/page3'
-          },
-          { 
-            name: 'rms/page4', title: '維修數據錄入', component: 'rms/page4'
-          },
-          {
-            name: 'rms/page5', title: '查詢', component: 'rms/page5'
-          }
+          { title: 'WIP trend & Quality feed back', path: 'page1' },
+          { title: 'WIP detail status', path: 'page2' },
+          { title: 'Check-in/out', path: 'page3' },
+          { title: '維修數據錄入', path: 'page4' },
+          { title: '查詢', path: 'page5' }
         ]
       },
-    ]
-  },
-  
-  {
-    title: 'Smart debug',
-    group: 'rms1',
-    component: 'rms',
-    icon: 'view_compact',
-    items: [
       {
-        name: '#', title: '不良位置參考', component: '#'
+        title: 'Smart debug',
+        group: '/smart_debug',
+        icon: 'view_compact',
+        items: [
+          { title: '不良位置參考', path: '#' },
+          { title: '智能維修步驟', path: '#' },
+          { title: '經驗分享', path: '#' }
+        ]
       },
       {
-        name: '#', title: '智能維修步驟', component: '#'
+        title: '維修績效管理',
+        group: '/kpi',
+        icon: 'view_compact',
+        items: [
+          { title: '按件計酬', path: '#' },
+          { title: 'ICT UPH', path: '#' },
+          { title: 'FT UPH', path: '#' },
+          { title: 'ICT返修率', path: '#' },
+          { title: 'FT 返修率', path: '#' }
+        ]
       },
       {
-        name: '#', title: '經驗分享', component: '#'
+        title: '重工管理',
+        group: '/rework',
+        icon: 'view_compact',
+        items: [
+          { title: 'PTH/BGA WIP 狀態&重工品質', path: '#' },
+          { title: 'WIP detail status', path: '#' },
+          { title: 'Check-in/out', path: '#' },
+          { title: '批量重工', path: '#' },
+          { title: '數據錄入', path: '#' },
+          { title: '查詢', path: '#' }
+        ]
+      },
+      {
+        title: '物料管理',
+        group: '/material_management',
+        icon: 'view_compact',
+        items: [
+          { title: '庫存狀態', path: 'stockstatus' },
+          { title: '維修員物料需求', path: 'usedstatus' },
+          { title: '物料員申請', path: 'assistantapply' },
+          { title: '維修員領料/退料', path: 'reneedorreturn' },
+          { title: '物料員退料', path: 'assistantreturn' },
+          { title: '报废管理', path: 'scrap' }
+        ]
+      },
+      {
+        title: '學習園地',
+        group: '/study_space',
+        icon: 'view_compact',
+        items: [
+          { title: '維修SOP', component: '#' },
+          { title: '客人文件', component: '#' },
+          { title: '業界標準', component: '#' },
+          { title: '客人稽核問題點追蹤', component: '#' },
+          { title: '其他', component: '#' }
+        ]
       }
     ]
-  },
-  {
-    title: '維修績效管理',
-    group: 'rms2',
-    component: 'rms',
-    icon: 'view_compact',
-    items: [
-      {
-        name: '#', title: '按件計酬', component: '#'
-      },
-      {
-        name: '#', title: 'ICT UPH', component: '#'
-      },
-      {
-        name: '#', title: 'FT UPH', component: '#'
-      },
-      {
-        name: '#', title: 'ICT返修率', component: '#'
-      },
-      {
-        name: '#', title: 'FT 返修率', component: '#'
-      }
-    ]
-  },
-  {
-    title: '重工管理',
-    group: 'rms3',
-    component: 'rms',
-    icon: 'view_compact',
-    items: [
-      {
-        name: '#', title: 'PTH/BGA WIP 狀態&重工品質', component: '#'
-      },
-      {
-        name: '#', title: 'WIP detail status', component: '#'
-      },
-      {
-        name: '#', title: 'Check-in/out', component: '#'
-      },
-      {
-        name: '#', title: '批量重工', component: '#'
-      },
-      {
-        name: '#', title: '數據錄入', component: '#'
-      },
-      {
-        name: '#', title: '查詢', component: '#'
-      }
-    ]
-  },
-  {
-    title: '物料管理',
-    group: 'rms4',
-    component: 'rms',
-    icon: 'view_compact',
-    items: [
-      {
-        name: 'rms/stockstatus', title: '庫存狀態', component: 'rms/stockstatus'
-      },
-      {
-        name: 'rms/usedstatus', title: '維修員物料需求', component: 'rms/usedstatus'
-      },
-      {
-        name: 'rms/assistantapply', title: '物料員申請', component: 'rms/assistantapply'
-      },
-      {
-        name: 'rms/reneedorreturn', title: '維修員領料/退料', component: 'rms/reneedorreturn'
-      },
-      {
-        name: 'rms/assistantreturn', title: '物料員退料', component: 'rms/assistantreturn'
-      },
-      {
-        name: 'rms/scrap', title: '报废管理', component: 'rms/scrap'
-      }
-    ]
-  },
-  {
-    title: '學習園地',
-    group: 'rms5',
-    component: 'rms',
-    icon: 'view_compact',
-    items: [
-      {
-        name: '#', title: '維修SOP', component: '#'
-      },
-      {
-        name: '#', title: '客人文件', component: '#'
-      },
-      {
-        name: '#', title: '業界標準', component: '#'
-      },
-      {
-        name: '#', title: '客人稽核問題點追蹤', component: '#'
-      },
-      {
-        name: '#', title: '其他', component: '#'
-      }
-    ]
-
   }
 ];
 
