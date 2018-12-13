6c32fb97fb97c737f6b889684cf7645572e4335f
M <f1331865@atm.systemplus.foxconn.com>
2018-12-13 10:50:57 +0800

    新加入库房管理系统

--- src/router/paths.js
+++ src/router/paths.js
@@ -1,11 +1,11 @@
 import defaultRouter from './default';
-import example from './example';
+// import example from './example';
 // import project from '../pages/project/path';
 import tipbu_6streams_path from '../pages/tipbu_6streams/path';
 import vision from '../pages/vision/router';
 import RMS from '../pages/RMS/path';
 import GreenConnect from '../pages/GreenConnect/path';
-// import vision3 from '../pages/vision3/router';
+import WarehouseManagement from '../pages/WarehouseManagement/path';
 import genius from '../pages/genius/router';
 
 const paths = [];
@@ -14,9 +14,9 @@ paths.push(...defaultRouter);
 paths.push(...tipbu_6streams_path);
 paths.push(...RMS);
 paths.push(...GreenConnect);
+paths.push(...WarehouseManagement);
 paths.push(...genius);
 paths.push(...vision);
-// paths.push(...vision3);
 // paths.push(...project);
 // paths.push(...example);
 
