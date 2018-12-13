d62afb1a65c82651c3b452f7f728bac6a07729f6
Jay <1032657951@qq.com>
2018-12-13 11:33:33 +0800

    合并库房管理系统

--- src/pages/WarehouseManagement/menu.js
+++ src/pages/WarehouseManagement/menu.js
@@ -4,40 +4,33 @@
 const menu = [
   {
     title: '庫房管理系統',   // 系统标题
-    group: 'Jay', 
-    component: 'Jay/page0',       
     icon: 'view_compact',
+    group: '/WMS', 
     // 定义二级菜单
     items: [
-      { 
-        // name: 'Jay/page1', 
-        title: '測試設備管理', 
-        group: 'Jay/TestDeviceManagement',
-        component: 'Jay/page1',
+      {
+        // name: 'Jay/page1',
+        title: '測試設備管理',
+        group: '',
         icon: 'view_compact',
         items: [
-          { title: '資產管理', path: '#' },
-          { name: 'Jay/page1', title: '校驗管理', component: '#' },
-          { name: 'Jay/page1', title: '保養管理', component: '#' },
+          { title: '資產管理', path: 'page1' },
+          { title: '校驗管理', path: 'page1' },
+          { title: '保養管理', path: 'page1' },
         ]
       },
       {
-        name: 'Jay/page2', 
-        title: '治具耗材管理', 
-        component: 'Jay/page2',
+        title: '治具耗材管理',
+        group: '',
         items: [
-          { name: 'Jay/page1', title: '線材管理', path: '404' },
-          { name: 'Jay/page1', title: 'SFP管理', path: '404' },
-          { name: 'Jay/page1', title: 'Tset治具管理', path: '404' },
-          { name: 'Jay/page1', title: '咨詢類管理', path: '404' },
-          { name: 'Jay/page1', title: '工具管理', path: '404' }
+          { title: '線材管理', path: '404' },
+          { title: 'SFP管理', path: '404' },
+          { title: 'Tset治具管理', path: '404' },
+          { title: '咨詢類管理', path: '404' },
+          { title: '工具管理', path: '404' }
         ]
       },
-      {
-        name: 'Jay/page3', 
-        title: 'Gold Board管理', 
-        component: 'Jay/page3'
-      },
+      { title: 'Gold Board管理', path: 'page3' },
     ]
   },
 ];
--- src/pages/WarehouseManagement/path.js
+++ src/pages/WarehouseManagement/path.js
@@ -1,64 +1,42 @@
 const router = [
   {
-    path: '/Jay/page0',
-    meta: { breadcrumb: true },
-    name: 'Jay/page0',
-    component: () => import(
-      /* webpackChunkName: "routes" */
-      /* webpackMode: "lazy-once" */
-      `./pages/MainInterface.vue`
-    )
-  },
-  {
-    path: '/Jay/page1',
-    meta: { breadcrumb: true },
-    name: 'Jay/page1',
-    component: () => import(
-      /* webpackChunkName: "routes" */
-      /* webpackMode: "lazy-once" */
-      `./pages/TestDeviceM.vue`
-    )
-  },
-  {
-    path: '/Jay/page2',
-    meta: { breadcrumb: true },
-    name: 'Jay/page2',
-    component: () => import(
-      /* webpackChunkName: "routes" */
-      /* webpackMode: "lazy-once" */
-      `./pages/Tool&ConsumablesM.vue`
-    )
-  },
-  {
-    path: '/Jay/page3',
-    meta: { breadcrumb: true },
-    name: 'Jay/page3',
-    component: () => import(
-      /* webpackChunkName: "routes" */
-      /* webpackMode: "lazy-once" */
-      `./pages/GoldBoardM.vue`
-    )
-  },
-  {
-    path: '/Jay/asset',
-    meta: { breadcrumb: true },
-    name: 'Jay/asset',
-    component: () => import(
-      /* webpackChunkName: "routes" */
-      /* webpackMode: "lazy-once" */
-      `./pages/GoldBoardM.vue`
-    )
-  },
-  // {
-  //   path: '/Jay/page5',
-  //   meta: { breadcrumb: true },
-  //   name: 'Jay/page5',
-  //   component: () => import(
-  //     /* webpackChunkName: "routes" */
-  //     /* webpackMode: "lazy-once" */
-  //     `./pages/UseInfoMaintenance.vue`
-  //   )
-  // },
+    path: '/WMS',
+    component: () => import('./WMS.vue'),
+    meta: {
+      theme: 'blue'
+    },
+    children: [
+      {
+        path: 'page0',
+        meta: { breadcrumb: true },
+        component: () => import(
+          /* webpackChunkName: "routes" */
+          /* webpackMode: "lazy-once" */
+          `./pages/MainInterface.vue`
+        )
+      },
+      {
+        path: 'page1',
+        meta: { breadcrumb: true },
+        component: () => import(`./pages/TestDeviceM.vue`)
+      },
+      {
+        path: 'page2',
+        meta: { breadcrumb: true },
+        component: () => import(`./pages/Tool&ConsumablesM.vue`)
+      },
+      {
+        path: 'page3',
+        meta: { breadcrumb: true },
+        component: () => import(`./pages/GoldBoardM.vue`)
+      },
+      {
+        path: 'asset',
+        meta: { breadcrumb: true },
+        component: () => import(`./pages/GoldBoardM.vue`)
+      },
+    ]
+  }
 ];
 
 export default router;
