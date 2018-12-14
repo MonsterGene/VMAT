c026861b551ce7bed7ea87cd9e4ef83ea6418c1c
M <f1331865@atm.systemplus.foxconn.com>
2018-12-14 10:21:59 +0800

    更新能源管理首页

--- src/pages/GreenConnect/api/config.js
+++ src/pages/GreenConnect/api/config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const serverAddr = 'http://10.167.192.146/SixStream/api';
+const serverAddr = 'http://10.167.192.233:8999';
 
 const catchMethod = function (error) {
   if (error.response) {
--- src/pages/GreenConnect/api/index.js
+++ src/pages/GreenConnect/api/index.js
@@ -1,9 +1,11 @@
 import demoApi from './demoApi';
 import pageApi from './pageApi';
 import barDateChart from './chartDemo';
+import homeApi from './home';
 
 export {
   demoApi,
   pageApi,
-  barDateChart
+  barDateChart,
+  homeApi
 };
