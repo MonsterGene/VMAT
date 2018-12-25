beed81512d5662835ef8a406a0a58b4911f8f3bf
M <f1331865@atm.systemplus.foxconn.com>
2018-12-25 15:01:53 +0800

    优化运行时间的显示

--- src/pages/tipbu_6streams/components/RunTime.vue
+++ src/pages/tipbu_6streams/components/RunTime.vue
@@ -63,21 +63,26 @@
         ['grid.left', '2%'],
         ['grid.bottom', '5%'],
         ['grid.right', '3%'],
+        ['grid.tooltip.trigger', 'axis'],
+        ['grid.tooltip.axisPointer.type', 'shadow'],
 
         ['series[0].type', 'bar'],
-        ['series[0].label.show', true],
+        ['series[0].label.show', false],
+        ['series[0].label.formatter', labelFormatter],
         ['series[0].smooth', true],
         ['series[0].stack', 1],
         ['series[0].label.position', 'top'],
         
         ['series[1].type', 'bar'],
-        ['series[1].label.show', true],
+        ['series[1].label.show', false],
+        ['series[1].label.formatter', labelFormatter],
         ['series[1].smooth', true],
         ['series[1].stack', 1],
         ['series[1].label.position', 'top'],
 
         ['series[2].type', 'bar'],
-        ['series[2].label.show', true],
+        ['series[2].label.show', false],
+        ['series[2].label.formatter', labelFormatter],
         ['series[2].smooth', true],
         ['series[2].stack', 1],
         ['series[2].label.position', 'top'],
@@ -107,6 +112,11 @@ export default {
       startDate: moment().subtract('months', 1).format('YYYY-MM-DD'),
       endDate: moment().format('YYYY-MM-DD'),
       chartData: { date: [] },
+      typeList: [
+        { key: 'run', value: '运行' },
+        { key: 'wait', value: '等待' },
+        { key: 'stop', value: '停止' }
+      ],
       lineList: [
         { text: '组装1线', value: 1 },
         { text: '组装2线', value: 2 },
@@ -126,6 +136,15 @@ export default {
     this.getChartData();
   },
   methods: {
+    labelFormatter (item) {
+      console.log(item);
+      const val = item.value[item.seriesIndex + 1];
+      if (Number(val) === 0) {
+        return '';
+      } else {
+        return val;
+      }
+    },
     getChartData () {
       // console.log(API.dailyData2(this.startDate, this.endDate));
       stationApi.getRunTime({
@@ -137,7 +156,17 @@ export default {
         const data = res.data;
         console.log(data);
         if (data.success) {
-          this.chartData = data.data;
+          const dataKeys = Object.keys(data.data);
+          const cData = {};
+          dataKeys.forEach(key => {
+            const keyMap = this.typeList.filter(v => v.key === key)[0];
+            if (keyMap) {
+              cData[keyMap.value] = data.data[key];
+            } else {
+              cData[key] = data.data[key];
+            }
+          });
+          this.chartData = cData;
           this.$nextTick(() => {
             this.$refs.runtime.update();
           });
