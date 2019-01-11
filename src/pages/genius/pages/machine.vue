0a2017c7249d9686522996df21f5988520a5b871
M <f1331865@atm.systemplus.foxconn.com>
2019-01-11 14:10:01 +0800

    demo api

--- src/pages/genius/pages/machine.vue
+++ src/pages/genius/pages/machine.vue
@@ -17,6 +17,7 @@
 
 <script>
 import MachineBrief from '../components/MachineBrief';
+import { machineApi } from '../api';
 
 export default {
   components: {
@@ -24,77 +25,17 @@ export default {
   },
   data () {
     return {
-      hosts: [
-        {
-          hostname: 'focwnbu1',
-          ipAddress: '10.167.219.23',
-          details: 'WNBU Genius Server',
-          profile: true,
-        },
-        {
-          hostname: 'focwnbu2',
-          ipAddress: '10.167.219.25',
-          details: 'WNBU Genius Server',
-          profile: true,
-        },
-        {
-          hostname: 'focwnbu3',
-          ipAddress: '10.167.219.27',
-          details: 'WNBU Genius Server',
-          profile: true,
-        },
-        {
-          hostname: 'focwnbu4',
-          ipAddress: '10.167.219.28',
-          details: 'WNBU Genius Server',
-          profile: true,
-        },
-        {
-          hostname: 'focwnbu5',
-          ipAddress: '10.167.219.30',
-          details: 'WNBU Genius Server',
-          profile: true,
-        },
-        {
-          hostname: 'focwnbu6',
-          ipAddress: '10.167.219.31',
-          details: 'WNBU Genius Server',
-          profile: true,
-        },
-        {
-          hostname: 'focwnbu7',
-          ipAddress: '10.167.219.33',
-          details: 'WNBU Genius Server',
-          profile: true,
-        },
-        {
-          hostname: 'focwnbu8',
-          ipAddress: '10.167.219.34',
-          details: 'WNBU Genius Server',
-          profile: true,
-        },
-        {
-          hostname: 'focwnbu9',
-          ipAddress: '10.167.219.35',
-          details: 'WNBU Genius Server',
-          profile: true,
-        },
-        {
-          hostname: 'focwnbu10',
-          ipAddress: '10.167.219.36',
-          details: 'WNBU Genius Server',
-          profile: true,
-        },
-        {
-          hostname: 'fortinet1',
-          ipAddress: '10.167.5.238',
-          details: 'Fortinet Genius Server',
-          profile: true,
-        },
-      ]
+      hosts: []
     };
   },
   methods: {
+    hosts () {
+      machineApi.hosts(null, true).then(res => {
+        if (res && res.status === 200) {
+          this.hosts = res.data;
+        }
+      });
+    }
   },
 };
 </script>
