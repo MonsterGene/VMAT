6ebf0431e58120394c3098358a13a4aba8006ceb
yang haoce <ronnie.lh.xiao@mail.foxconn.com>
2019-01-11 09:20:16 +0800

    Update src/pages/tipbu_6streams/menu.js

--- src/pages/tipbu_6streams/menu.js
+++ src/pages/tipbu_6streams/menu.js
@@ -2,7 +2,29 @@ const menu_6streams_tipbu = [
   {
     title: 'NPBG-龙华-MFG3',
     icon: 'view_compact',
-    path: '/tipbu-6streams'
+    group: '/tipbu-6streams',
+    items: [
+      { title: '组装线', path: 'packing' },
+      {
+        title: '测试自动化',
+        group: 'auto_test',
+        items: [
+          // { title: '智能监控', path: 'intelligent_surveillance' },
+          { title: '智能监控', path: 'line' },
+          { title: '状态看板', path: 'status_board' },
+          {
+            title: '良率跟踪',
+            group: 'yield_watch',
+            items: [
+              { title: 'AT', path: 'at' },
+              { title: 'Run In', path: 'run_in' }
+            ]
+          },
+          { title: '设备维护', path: 'facilities_maintenance' },
+          { title: 'Robot', path: 'robot' }
+        ]
+      }
+    ]
   }
 ];
 
